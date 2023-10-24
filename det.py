3d4b3a69c7994219a666bea243743ea6d9f9ed1b
voidkyun <d12e93@outlook.jp>
2023-10-24 19:36:59 +0900

    deleteheap

--- det.py
+++ det.py
@@ -1,4 +1,26 @@
-import heap
+def heap(n):
+	"""
+	heapのアルゴリズムでSnを生成
+	"""
+	A=[i for i in range(n)]
+	S=[]
+	def swap(a,b):
+		temp=A[b]
+		A[b]=A[a]
+		A[a]=temp
+	def generate(m):
+		if m==1:
+			_A=A.copy()
+			S.append(_A)
+		else:
+			for i in range(m):
+				generate(m-1)
+				if m%2==0:
+					swap(i,m-1)
+				else:
+					swap(0,m-1)
+	generate(n)
+	return(S)
 
 def sgn(l):
 	count=0
@@ -10,7 +32,7 @@ def sgn(l):
 
 def det(A):
 	n=len(A[0])
-	Sn=heap.heap(n)
+	Sn=heap(n)
 	determinant=0
 	for s in Sn:
 		temp=sgn(s)
